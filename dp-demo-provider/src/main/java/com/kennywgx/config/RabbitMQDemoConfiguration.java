4e22f4fc927c6fda5cc9766ee720b374f3868f62
kenny <543444431@qq.com>
2020-04-02 19:30:53 +0800

    fix: AmqpException No method found for class B

--- dp-demo-provider/src/main/java/com/kennywgx/config/RabbitMQDemoConfiguration.java
+++ dp-demo-provider/src/main/java/com/kennywgx/config/RabbitMQDemoConfiguration.java
@@ -4,6 +4,9 @@ import com.kennywgx.service.MQDemoService;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +20,15 @@ public class RabbitMQDemoConfiguration {
         return new Queue("test.queue");
     }
 
-    @RabbitListener(queues = "test.queue")
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
+    }
+
+    @RabbitListener(queues = "test.queue", containerFactory = "rabbitListenerContainerFactory")
     public class Receiver {
         @Autowired
         private MQDemoService service;
